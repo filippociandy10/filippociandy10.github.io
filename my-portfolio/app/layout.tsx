e538f21e5601a034690b9e1a68111b92f7fda1c1
atlas <filippociandy@Filippos-Air.lan>
2025-02-18 17:02:35 -0700

    initialized project

---
+++ my-portfolio/app/layout.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-900 text-white">
+        {children}
+      </body>
+    </html>
+  );
+}
